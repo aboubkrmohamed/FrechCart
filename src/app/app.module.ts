42a34d436494d7340b3fbf30cdc759be9755a412
Aboubkr <aboubkrmohamed86@gmail.com>
2023-10-19 18:24:03 +0200

    f

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { RegisterComponent } from './register/register.component';
 import { ProductsComponent } from './products/products.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { ProductsComponent } from './products/products.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import{FormGroup,FormControl,Validators} from '@angular/forms'
+
 
 @Component({
   selector: 'app-register',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  registerForm:FormGroup=new FormGroup({
+     name:new FormControl(null ,[Validators.required ,Validators.minLength(3),Validators.maxLength(15)]),
+     email:new FormControl(null ,[Validators.required,Validators.email]),
+     password:new FormControl(null ,[Validators.required,Validators.pattern(/^[A-Z][a-z0-9]{5,10}$/)]),
+     rePassword:new FormControl(null ,[Validators.required,Validators.pattern(/^[A-Z][a-z0-9]{5,10}$/)]),
+     phone:new FormControl(null,[Validators.required,Validators.pattern(/^01[0125][0-9]{8}/)]),
+  })
+
+  handelRegister(registerForm:FormGroup)
+  {
+    alert("DONE")
+    console.log(registerForm)
+  }
 }
